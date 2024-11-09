04df5160fd767b6c3dacc239ebaa15d9f8a25f9b
Yashi Singh <136097732+YashiSingh158@users.noreply.github.com>
2024-11-09 10:32:23 +0530

    app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
+declare let dataLayer: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Task-API-Weather';
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        dataLayer.push({
+          event: 'pageview',
+          pagePath: event.urlAfterRedirects,
+        });
+        console.log('GTM pageview event:', event.urlAfterRedirects);
+      }
+    });
+  }
 }
