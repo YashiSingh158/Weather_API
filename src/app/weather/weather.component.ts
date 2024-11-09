f87a74cb0d610799d155bc2ded04a65e2ce9b037
Yashi Singh <136097732+YashiSingh158@users.noreply.github.com>
2024-11-09 10:37:52 +0530

    weather.component.ts

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -21,31 +21,13 @@ export class WeatherComponent implements OnInit {
   }
 
   // Method triggered when a country is selected
-  // onCountrySelect(): void {
-  //   if (this.selectedCountry) {
-  //     this.countryWeatherService.getWeather(this.selectedCountry).subscribe(data => {
-  //       this.weatherData = data;
-  //     });
-  //   }
-  // }
-
-  onCountrySelect(): void 
-  {
+  onCountrySelect(): void {
     if (this.selectedCountry) {
       this.countryWeatherService.getWeather(this.selectedCountry).subscribe(data => {
         this.weatherData = data;
-  
-        // Push event to dataLayer
-        window.dataLayer.push({
-          event: 'weatherDataLoaded',
-          country: this.selectedCountry,
-          temperature: this.weatherData.main.temp - 273.15,
-          weatherDescription: this.weatherData.weather[0].description,
-        });
-  
-        console.log('Weather data loaded event pushed for:', this.selectedCountry);
       });
     }
   }
+
 
 }
